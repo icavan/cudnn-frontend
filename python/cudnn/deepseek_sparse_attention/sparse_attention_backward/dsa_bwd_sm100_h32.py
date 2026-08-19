73094cb7bb461ea15205e1c097364972f06a4302
shuyan.ycf <shuyan.ycf@antgroup.com>
2026-08-19 21:31:32 +0800

    Convert H32 P and dS directly at SMEM store
    Signed-off-by: shuyan.ycf <shuyan.ycf@antgroup.com>

--- python/cudnn/deepseek_sparse_attention/sparse_attention_backward/dsa_bwd_sm100_h32.py
+++ python/cudnn/deepseek_sparse_attention/sparse_attention_backward/dsa_bwd_sm100_h32.py
@@ -92,8 +92,8 @@ class FlashAttentionDSABackwardSm100H32(FlashAttentionDSABackwardSm100H16):
         self.tmem_dKV4_offset = 352
 
         self.num_regs_load_KV = 40
-        self.num_regs_compute = 160
-        self.num_regs_reduce = 72
+        self.num_regs_compute = 128
+        self.num_regs_reduce = 88
 
     def _setup_attributes(self):
         super()._setup_attributes()
@@ -446,8 +446,6 @@ class FlashAttentionDSABackwardSm100H32(FlashAttentionDSABackwardSm100H16):
         tTR_tdP = thr_t2r_dP.partition_S(tdPtdP)
         tTR_rS = cute.make_rmem_tensor(tTR_cS.shape, self.acc_dtype)
         tTR_rdP = cute.make_rmem_tensor(tTR_cdP.shape, self.acc_dtype)
-        tTR_rS_f16 = cute.make_rmem_tensor(tTR_cS.shape, self.element_dtype)
-        tTR_rdP_f16 = cute.make_rmem_tensor(tTR_cdP.shape, self.element_dtype)
 
         load_compute_LSE_pipeline.consumer_wait(load_compute_LSE_consumer_state)
         load_compute_sum_OdO_pipeline.consumer_wait(load_compute_sum_OdO_consumer_state)
@@ -475,7 +473,6 @@ class FlashAttentionDSABackwardSm100H32(FlashAttentionDSABackwardSm100H16):
                 )
                 tTR_rS[i] = cute.math.exp2(tTR_rS[i], fastmath=True)
                 tTR_rS[i + 1] = cute.math.exp2(tTR_rS[i + 1], fastmath=True)
-            tTR_rS_f16 = self.quantize(tTR_rS, 2)
 
             cute.copy(tiled_t2r_dP, tTR_tdP, tTR_rdP)
             for i in cutlass.range(0, cute.size(tTR_rdP), 2, unroll_full=True):
@@ -490,7 +487,6 @@ class FlashAttentionDSABackwardSm100H32(FlashAttentionDSABackwardSm100H16):
                     (tTR_rdP[i], tTR_rdP[i + 1]),
                     (tTR_rS[i], tTR_rS[i + 1]),
                 )
-            tTR_rdP_f16 = self.quantize(tTR_rdP, 2, scale_softmax)
 
             cute.arch.fence_view_async_tmem_load()
             self.compute_sync_barrier.arrive_and_wait()
@@ -499,24 +495,24 @@ class FlashAttentionDSABackwardSm100H32(FlashAttentionDSABackwardSm100H16):
                 kv_half = self.num_kv_subtiles - 1 - half_iter
                 compute_mma_P_pipeline.producer_acquire(compute_mma_P_producer_state)
                 p_stage = 0 if self.compute_mma_P_stage == 1 else compute_mma_P_producer_state.index
-                for i in cutlass.range_constexpr(cute.size(tTR_rS_f16)):
+                for i in cutlass.range_constexpr(cute.size(tTR_rS)):
                     global_row = cute.get(tTR_cS[i], mode=[0])
                     if global_row // self.kv_subtile == kv_half:
                         row = global_row - kv_half * self.kv_subtile
                         col = cute.get(tTR_cS[i], mode=[1])
-                        sP[(row, col), 0, 0, p_stage] = tTR_rS_f16[i]
+                        sP[(row, col), 0, 0, p_stage] = self.element_dtype(tTR_rS[i])
                 cute.arch.fence_proxy("async.shared", space="cta")
                 compute_mma_P_pipeline.producer_commit(compute_mma_P_producer_state)
                 compute_mma_P_producer_state.advance()
 
                 compute_mma_dS_pipeline.producer_acquire(compute_mma_dS_producer_state)
                 ds_stage = 0 if self.compute_mma_dS_stage == 1 else compute_mma_dS_producer_state.index
-                for i in cutlass.range_constexpr(cute.size(tTR_rdP_f16)):
+                for i in cutlass.range_constexpr(cute.size(tTR_rdP)):
                     global_row = cute.get(tTR_cdP[i], mode=[0])
                     if global_row // self.kv_subtile == kv_half:
                         row = global_row - kv_half * self.kv_subtile
                         col = cute.get(tTR_cdP[i], mode=[1])
-                        sdS[(row, col), 0, 0, ds_stage] = tTR_rdP_f16[i]
+                        sdS[(row, col), 0, 0, ds_stage] = self.element_dtype(tTR_rdP[i] * scale_softmax)
                 cute.arch.fence_proxy("async.shared", space="cta")
                 compute_mma_dS_pipeline.producer_commit(compute_mma_dS_producer_state)
                 compute_mma_dS_producer_state.advance()
